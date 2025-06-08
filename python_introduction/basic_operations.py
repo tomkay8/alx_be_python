e72ea3825b8b47a56645788c3900788033be5660
Kayode Thomas <thomaskayode8@gmail.com>
2025-06-08 23:30:25 +0100

    added two .py files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+a = int(number1)
+b = int(number2)
+
+add = a + b
+diff = a - b
+prod = a * b
+
+print('Addition of ',number1, 'and ',number2, 'is ',add)
+print('Subtraction of ',number1, 'and ',number2, 'is ',diff)
+print('Multiplication of ',number1, 'and ',number2, 'is ',prod)
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = float(principal * rate * time)
+
+print('The simple interest is: ',interest)
