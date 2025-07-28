a77b7483c98db1f0dbc5ff53382248e5919ae9dc
Kayode Thomas <thomaskayode8@gmail.com>
2025-07-28 06:01:34 +0100

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,38 @@
+shopping_list = []
+
+while True:
+    print("\nShopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        item = input('Please select what you want: ')
+        shopping_list.append(item)
+
+    elif choice == '2':
+        item = input('Enter the item to remove: ')
+        if item in shopping_list:
+            shopping_list.remove(item)
+            print(f"{item} removed from the list.")
+        else:
+            print(f"{item} is not in the shopping list.")
+
+    elif choice == '3':
+        if not shopping_list:
+            print("Your shopping list is empty.")
+        else:
+            print("Your shopping list:")
+            for item in shopping_list:
+                print(f"- {item}")
+
+    elif choice == '4':
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
+
