ca072763cadc7ff6f0904dc8c74a527a484ac970
Kayode Thomas <thomaskayode8@gmail.com>
2025-08-17 14:52:56 +0100

    Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        # Start with initial balance (default = 0)
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        """Add money to the account"""
+        if amount > 0:
+            self.account_balance += amount
+
+    def withdraw(self, amount):
+        """Take money out if funds are enough"""
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        """Show the current balance"""
+        print(f"Current Balance: ${self.account_balance}")        
